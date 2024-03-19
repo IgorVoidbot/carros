19321129c279033a97e2dc745beaf148844ef3a8
Igordanilo03 <141951957+Igordanilo03@users.noreply.github.com>
2024-03-19 13:33:49 -0300

    atualização psycopg2 para psycopg2-binary

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h-k4noqy+d#-1ab__fjtbwxx&x!km27)d6u5^xj!+50xn8575v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
